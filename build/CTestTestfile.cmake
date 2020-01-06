6591d49937383094010735eb31af091baf52a494
Haoran-Zhao <zhaohaorandl@gmail.com>
2020-01-06 15:01:50 -0600

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/haoran/cubes/src
+# Build directory: /home/haoran/cubes/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(cubes_simulation)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/haoran/cubes/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/haoran/cubes/devel;/home/haoran/US_UR3/devel;/opt/ros/kinetic".split(';') if "/home/haoran/cubes/devel;/home/haoran/US_UR3/devel;/opt/ros/kinetic" != "" else []
